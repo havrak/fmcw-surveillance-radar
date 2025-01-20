bafb04b276158eea448f41aad80ed2024fdf01bd
Havra <krystof@havrak.xyz>
2025-01-20 17:16:54 +0100

    Nuked miniproject, moved doc to main folder and started a rewrite of it

--- matlab/app.m
+++ matlab/app.m
@@ -1,4 +1,4 @@
-classdef app < handle 
+classdef app < handle
 	properties (Access = public)
 		% GUI %
 		hPanelBtn              % uipanel - panel to group buttons
@@ -9,13 +9,13 @@ classdef app < handle
 		h
 
 
-		% OTHER VARS% 
-		hPreferences;
-		hPlatformControl;
+		% OTHER VARS%
+		hPreferences (1,1) preferences = Preferences.empty;
+		hPlatformControl (1,1) platformControl = PlatformContro.empty;
 		hRadar;
 
-		hFig; 
-    hToolbar; 
+		hFig;
+    hToolbar;
 	end
 
 	methods(Access=private)
@@ -25,13 +25,13 @@ classdef app < handle
 			obj.hPlatformControl = platformControl(obj.hPreferences);
 			obj.hRadar = radar(obj.hPreferences);
 		end
-		
+
 		function shutdown(obj)
 			fprintf("App | shutdown\n")
 			obj.hRadar.endProcesses();
 			obj.hPlatformControl.endProcesses();
 		end
-		
+
 		function constructGUI(obj)
 			figSize = [1200, 800];
 			screenSize = get(groot, 'ScreenSize');
@@ -43,18 +43,18 @@ classdef app < handle
 				'Visible', 'on', ...
 				'AutoResizeChildren', 'off', ...
 				'DeleteFcn',@(~,~) obj.shutdown());
-			
+
 			obj.hToolbar = uitoolbar(obj.hFig);
 
 			[img,map] = imread(fullfile(matlabroot,...
 				'toolbox','matlab','icons','profiler.gif'));
 			ptImage = ind2rgb(img,map);
-			
+
 			uipushtool(obj.hToolbar, ...
 				'TooltipString', 'Preferences', ...
 				'ClickedCallback', @(~,~) obj.hPreferences.showGUI(), ...
 				 'CData', ptImage); % Example icon
-			
+
 			[img,map] = imread(fullfile(matlabroot,...
 				'toolbox','matlab','icons','tool_rotate_3d.gif'));
 			ptImage = ind2rgb(img,map);
@@ -63,12 +63,12 @@ classdef app < handle
 				'TooltipString', 'Platform', ...
 				'ClickedCallback', @(~,~) obj.hPlatformControl.showGUI(), ...
 				 'CData', ptImage); % Example icon
-			
-		
+
+
 			[img,map] = imread(fullfile(matlabroot,...
 				'toolbox','matlab','icons','helpicon.gif'));
 			ptImage = ind2rgb(img,map);
-			
+
 			uipushtool(obj.hToolbar, ...
 				'TooltipString', 'Help', ...
 				'CData', ptImage);
@@ -77,14 +77,14 @@ classdef app < handle
 				'Title', 'Actions', ...
 				'Tag', 'ButtonPanel', ...
 				'Units', 'pixels');
-			
+
 			obj.hTextTelemetry =  uicontrol('Style', 'edit', ...
 				'Parent', obj.hFig, ...
 				'Tag', 'ProgramDisplay', ...
 				'Max', 2, ...
 				'HorizontalAlignment', 'left', ...
 				'String', 'Platform serial output');
-			
+
 			obj.hBtnConnectPlatform = uicontrol('Style', 'pushbutton', ...
 				'Parent', obj.hPanelBtn, ...
 				'String', 'Platform Connect', ...
@@ -103,7 +103,7 @@ classdef app < handle
 		end
 
 		function setupRadarSerial()
-			
+
 		end
 
 		function resizeUI(obj)
@@ -112,21 +112,21 @@ classdef app < handle
 			height = figPos(4);
 
 			displayWidth = width - 180;
-			
+
 			obj.hTextTelemetry.Position = [10, 20, displayWidth-20, 200];
-			
+
 			buttonPanelWidth = 180;
 			obj.hPanelBtn.Position = [displayWidth, 20, buttonPanelWidth-10, height - 30];
 			buttonHeight = 40;
 			spacing = 10;
 			obj.hBtnConnectPlatform.Position = [10, height - 50 - buttonHeight - spacing, buttonPanelWidth - 30, buttonHeight];
 			obj.hBtnConnectRadar.Position = [10, height - 50 - 2 * (buttonHeight + spacing), buttonPanelWidth - 30, buttonHeight];
-			
+
 		end
 
 	end
 
-	
+
 	methods(Static=true)
 
 		function handle=getInstance()
